28983dea974304ca18c4a6cec34b9465ccb30628
77f315 <77f315@gmail.com>
2025-10-28 05:52:34 +0200

    removed get all users endpoint

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -4,7 +4,6 @@ import { authenticate } from '../middleware/auth';
 
 async function userRoutes(fastify: FastifyInstance) {
     // Get all users (protected route)
-    fastify.get('/users', { preHandler: authenticate }, userController.getAllUsers.bind(userController));
 }
 
 export default userRoutes;
