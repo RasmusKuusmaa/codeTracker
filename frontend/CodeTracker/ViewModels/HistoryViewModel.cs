020c02df315c883ab333c57ccd1e659a1d5ec926
77f315 <77f315@gmail.com>
2025-11-03 05:08:35 +0200

    show sessions in hsitory

---
+++ frontend/CodeTracker/ViewModels/HistoryViewModel.cs
@@ -0,0 +1,46 @@
+﻿using CodeTracker.Models;
+using CodeTracker.Service;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTracker.ViewModels
+{
+    public class HistoryViewModel : BaseViewModel
+    {
+        private readonly DashBoardService _dashBoardService = new DashBoardService();
+
+        private ObservableCollection<Session> _sessions;
+
+        public ObservableCollection<Session> Sessions
+        {
+            get { return _sessions; }
+            set
+            {
+                _sessions = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public HistoryViewModel()
+        {
+            Sessions = new ObservableCollection<Session>();
+            _ = LoadSessionsAsync();
+        }
+
+        private async Task LoadSessionsAsync()
+        {
+            var sessions = await _dashBoardService.GetSessionsAsync();
+            Sessions.Clear();
+            foreach (var session in sessions)
+            {
+                Sessions.Add(session);
+            }
+
+        }
+    }
+}
